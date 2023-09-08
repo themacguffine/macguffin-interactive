081742953796be463105078edb573d89dfb1c189
Ilkin <105630796+ilkin0316@users.noreply.github.com>
2023-09-08 22:56:20 +0400

    Animations
    Animations

---
+++ NinjaWay/NinjaWay/Assets/Scripts/DeathDetection.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityStandardAssets.Characters.FirstPerson
+{
+    public class DeathDetection : MonoBehaviour {
+
+        public FirstPersonController fpc;
+        public GameObject deathText;
+		public AudioClip deathSound;
+		private AudioSource audioSource;
+
+        void OnControllerColliderHit(ControllerColliderHit hit) {
+            if (hit.gameObject.tag.Equals("KillZone") && !fpc.isDead)
+            {
+                deathText.SetActive(true);
+                if (!fpc.jumpCommand.Equals("")) {
+                    fpc.jumpCommand = "";
+                }
+                fpc.moveCommand = "";
+                fpc.isDead = true;
+				PlayerPrefs.SetInt("highestScore", (int)transform.position.x);
+				audioSource = GetComponent<AudioSource>();
+				audioSource.clip = deathSound;
+				audioSource.Play ();
+            }
+        }
+    }
+}
